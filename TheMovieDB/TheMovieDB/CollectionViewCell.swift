dca2c1a2538b6a4e5694e6d1cfc451597d1391c0
Diego Alejandro Villa Cardenas <d.villa@co-it00998.globant.com>
2018-08-15 11:46:21 -0500

    started to handle the parsed information of the API and started to work in the movies layout

---
+++ TheMovieDB/TheMovieDB/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  TheMovieDB
+//
+//  Created by Diego Alejandro Villa Cardenas on 8/15/18.
+//  Copyright © 2018 Globant. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var movieImage: UIImageView!
+}
--- TheMovieDB/TheMovieDB/ViewController.swift
+++ TheMovieDB/TheMovieDB/ViewController.swift
@@ -8,17 +8,33 @@
 
 import UIKit
 import Alamofire
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    @IBOutlet weak var collectionView: UICollectionView!
+    static let kURL = "https://api.themoviedb.org/3/movie/top_rated?api_key=1f4d7de5836b788bdfd897c3e0d0a24b&language=en-US&page=1"
+    static let kResults = "results"
+    var movies: [[String: Any]] = [[String: Any]]()
+    
+    let test = [#imageLiteral(resourceName: "iTunesArtwork"),#imageLiteral(resourceName: "iTunesArtwork"),#imageLiteral(resourceName: "iTunesArtwork"),#imageLiteral(resourceName: "iTunesArtwork")]
+    
+    let testImage: [UIImage] = [
+        UIImage(named: "iTunesArtwork")!,
+        UIImage(named: "iTunesArtwork")!,
+        UIImage(named: "iTunesArtwork")!,
+        UIImage(named: "iTunesArtwork")!
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        Alamofire.request("https://api.themoviedb.org/3/movie/top_rated?api_key=1f4d7de5836b788bdfd897c3e0d0a24b&language=en-US&page=1").responseJSON { response in
-            // print(response)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        Alamofire.request(ViewController.kURL).responseJSON { response in
             if let responseJSON = response.result.value {
-                let data: Dictionary = responseJSON as! Dictionary<String, Any>
-                print(data)
+                let dataResults = responseJSON as! [String: Any]                 //  what's the difference between let data: Dictionary = responseJSON as! Dictionary<String, Any>
+                let moviesData = dataResults[ViewController.kResults] as! [[String: Any]]
+                self.movies = moviesData
+                print(self.movies[0])
             }
-            
         }
         
     }
@@ -26,6 +42,16 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return test.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        cell.movieImage.image = test[indexPath.item]
+        return cell
     }
 
 
