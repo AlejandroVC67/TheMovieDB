8adac741e2259746f1fbfffb7891a01913a0daac
Diego Alejandro Villa Cardenas <d.villa@co-it00998.globant.com>
2018-08-15 12:10:42 -0500

    finished rendering the images, still need to look for code upgrades

--- TheMovieDB/TheMovieDB/ViewController.swift
+++ TheMovieDB/TheMovieDB/ViewController.swift
@@ -13,15 +13,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     static let kURL = "https://api.themoviedb.org/3/movie/top_rated?api_key=1f4d7de5836b788bdfd897c3e0d0a24b&language=en-US&page=1"
     static let kResults = "results"
     var movies: [[String: Any]] = [[String: Any]]()
-    
-    let test = [#imageLiteral(resourceName: "iTunesArtwork"),#imageLiteral(resourceName: "iTunesArtwork"),#imageLiteral(resourceName: "iTunesArtwork"),#imageLiteral(resourceName: "iTunesArtwork")]
-    
-    let testImage: [UIImage] = [
-        UIImage(named: "iTunesArtwork")!,
-        UIImage(named: "iTunesArtwork")!,
-        UIImage(named: "iTunesArtwork")!,
-        UIImage(named: "iTunesArtwork")!
-    ]
+    var moviesImages: [UIImage] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,13 +22,12 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
         Alamofire.request(ViewController.kURL).responseJSON { response in
             if let responseJSON = response.result.value {
-                let dataResults = responseJSON as! [String: Any]                 //  what's the difference between let data: Dictionary = responseJSON as! Dictionary<String, Any>
+                let dataResults = responseJSON as! [String: Any]                 //what's the difference between let data: Dictionary = responseJSON as! Dictionary<String, Any>
                 let moviesData = dataResults[ViewController.kResults] as! [[String: Any]]
                 self.movies = moviesData
-                print(self.movies[0])
+                self.loadImages()
             }
         }
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,16 +35,26 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         // Dispose of any resources that can be recreated.
     }
     
+    func loadImages() {
+        let kImageURL = "https://image.tmdb.org/t/p/w500"
+        for movie in self.movies {
+            // print(movie["poster_path"]!)
+            let imageUrl = URL(string: kImageURL + String(describing: movie["poster_path"]!))!
+            let imageData = try! Data(contentsOf: imageUrl)
+            self.moviesImages.append(UIImage(data: imageData)!)
+            collectionView.reloadData()
+        }
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return test.count
+        return self.moviesImages.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
-        cell.movieImage.image = test[indexPath.item]
+        cell.movieImage.image = self.moviesImages[indexPath.item]
         return cell
     }
-
-
+    
 }
 
