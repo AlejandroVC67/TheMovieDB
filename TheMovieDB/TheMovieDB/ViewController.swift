b37491e844967b10d703de23e7000a8d70f1ac12
Diego Alejandro Villa Cardenas <d.villa@co-it00998.globant.com>
2018-08-15 09:09:40 -0500

    started the alamo request and json parsing

--- TheMovieDB/TheMovieDB/ViewController.swift
+++ TheMovieDB/TheMovieDB/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
-
+import Alamofire
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        Alamofire.request("https://api.themoviedb.org/3/movie/top_rated?api_key=1f4d7de5836b788bdfd897c3e0d0a24b&language=en-US&page=1").responseJSON { response in
+            // print(response)
+            if let responseJSON = response.result.value {
+                let data: Dictionary = responseJSON as! Dictionary<String, Any>
+                print(data)
+            }
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
