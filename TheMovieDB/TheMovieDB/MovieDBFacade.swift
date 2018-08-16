484dd009cd23a29a8c7d9c92bebda4868f37f417
Diego Alejandro Villa Cardenas <d.villa@co-it00998.globant.com>
2018-08-16 15:55:36 -0500

    deleted unnecessary file MovieDBRetriever and moved the code to MovieDBFacade

--- TheMovieDB/TheMovieDB/MovieDBFacade.swift
+++ TheMovieDB/TheMovieDB/MovieDBFacade.swift
@@ -10,10 +10,16 @@ import Foundation
 import Alamofire
 
 class MovieDBFacade {
-    
+    static let kURL = "https://api.themoviedb.org/3/movie/top_rated?api_key=1f4d7de5836b788bdfd897c3e0d0a24b&language=en-US&page=1"
+    static let kResults = "results"
     static func RetrieveTopRated (completion: @escaping (TopRated) -> Void) {
-        MovieDBRetriver.RetrieveTopRatedMovies() { response in
-            completion(TopRated(json: response))
+        Alamofire.request(MovieDBFacade.kURL).responseJSON { response in
+            if let responseJSON = response.result.value {
+                let dataResults = responseJSON as! [String: Any]
+                completion(TopRated(json: dataResults))
+            } else {
+                print("Couldn't get data")
+            }
         }
     }
 }
