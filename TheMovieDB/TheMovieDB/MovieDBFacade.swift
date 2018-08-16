adba393976009f08314bc3239e079ea1301379e8
Diego Alejandro Villa Cardenas <d.villa@co-it00998.globant.com>
2018-08-16 16:08:27 -0500

    added manual response validation

--- TheMovieDB/TheMovieDB/MovieDBFacade.swift
+++ TheMovieDB/TheMovieDB/MovieDBFacade.swift
@@ -13,13 +13,19 @@ class MovieDBFacade {
     static let kURL = "https://api.themoviedb.org/3/movie/top_rated?api_key=1f4d7de5836b788bdfd897c3e0d0a24b&language=en-US&page=1"
     static let kResults = "results"
     static func RetrieveTopRated (completion: @escaping (TopRated) -> Void) {
-        Alamofire.request(MovieDBFacade.kURL).responseJSON { response in
-            if let responseJSON = response.result.value {
-                let dataResults = responseJSON as! [String: Any]
-                completion(TopRated(json: dataResults))
-            } else {
-                print("Couldn't get data")
+        Alamofire.request(MovieDBFacade.kURL).validate(statusCode: 200..<500).responseJSON { response in
+            switch response.result {
+                case .success:
+                    if let responseJSON = response.result.value {
+                        let dataResults = responseJSON as! [String: Any]
+                        completion(TopRated(json: dataResults))
+                    } else {
+                        print("Couldn't get data")
+                    }
+                case .failure(let error):
+                    print(error)
             }
+
         }
     }
 }
